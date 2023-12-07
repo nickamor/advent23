3e017fbde77c984c6617018510ac6f7b6f5bfa0b
Nicholas Amor <me@nickamor.com>
2023-12-07 19:16:08 +1100

    day07 step 1

--- day07/main.py
+++ day07/main.py
@@ -2,7 +2,7 @@ import re
 
 
 def read_file(filename):
-    with open(filename, 'r') as f:
+    with open(filename, "r") as f:
         return list(filter(lambda l: len(l) > 0, f.read().split("\n")))
 
 
@@ -14,60 +14,75 @@ def parse_line(line):
 def parse_input(lines):
     return list(map(parse_line, lines))
 
+
 label_strength = {
-    'A': 13, 'K': 12, 'Q': 11, 'J': 10, 'T': 9, '9': 8, '8': 7, '7': 6, '6': 5, '5': 4, '4': 3, '3': 2, '2': 1
+    "A": "A",
+    "K": "B",
+    "Q": "C",
+    "J": "D",
+    "T": "E",
+    "9": "F",
+    "8": "G",
+    "7": "H",
+    "6": "I",
+    "5": "J",
+    "4": "K",
+    "3": "L",
+    "2": "M",
 }
+
 
 def hand_ranking(line):
     [hand, _wager] = line
     counts = {
-        'A': 0,
-        'K': 0,
-        'Q': 0,
-        'J': 0,
-        'T': 0,
-        '9': 0,
-        '8': 0,
-        '7': 0,
-        '6': 0,
-        '5': 0,
-        '4': 0,
-        '3': 0,
-        '2': 0
+        "A": 0,
+        "K": 0,
+        "Q": 0,
+        "J": 0,
+        "T": 0,
+        "9": 0,
+        "8": 0,
+        "7": 0,
+        "6": 0,
+        "5": 0,
+        "4": 0,
+        "3": 0,
+        "2": 0,
     }
-    base_score = 0
-    for c in hand:
-        counts[c] += 1
-        base_score += label_strength[c] << 1
-    type_score = 0
-    cc = sorted(counts.items(), key=lambda i: -i[1])
-    if cc[0][1] == 5:
-        # Five of a kind
-        type_score = 7
-    if cc[0][1] == 4:
-        # Four of a kind
-        type_score = 6
-    if cc[0][1] == 3 and cc[1][1]:
-        # Full house
-        type_score = 5
-    if cc[0][1] == 3:
-        # Three of a kind
-        type_score = 4
-    if cc[0][1] == 2 and cc[1][1] == 2:
-        # Two pair
-        type_score = 3
-    if cc[0][1] == 2:
-        # One pair
-        type_score = 2
-    if cc[0][1] == 1:
-        # High card
-        type_score = 1
+    base_score = ""
+    for card in hand:
+        counts[card] += 1
+        base_score += label_strength[card]
 
-    return base_score << type_score
+    hand_type = ""
+    counted = sorted(counts.items(), key=lambda i: -i[1])
+    if counted[0][1] == 5:
+        # Five of a kind
+        hand_type = "A"
+    elif counted[0][1] == 4:
+        # Four of a kind
+        hand_type = "B"
+    elif counted[0][1] == 3 and counted[1][1] == 2:
+        # Full house
+        hand_type = "C"
+    elif counted[0][1] == 3:
+        # Three of a kind
+        hand_type = "D"
+    elif counted[0][1] == 2 and counted[1][1] == 2:
+        # Two pair
+        hand_type = "E"
+    elif counted[0][1] == 2:
+        # One pair
+        hand_type = "F"
+    else:
+        # High card
+        hand_type = "G"
+
+    return hand_type + base_score
 
 
 def score_ranking(input):
-    s = sorted(input, key=hand_ranking)
+    s = sorted(input, key=hand_ranking, reverse=True)
     out = []
     for i, hand in enumerate(s):
         out.append((hand, (i + 1) * hand[1]))
@@ -84,9 +99,9 @@ def reduce_total(input):
 def main(filename="./day07/test"):
     lines = read_file(filename)
     input = parse_input(lines)
-    # input = list(map(lambda l: (l, hand_ranking(l), input)))
+    # print(list(map(lambda l: (l, hand_ranking(l)), input)))
     print(reduce_total(score_ranking(input)))
 
 
 main()
-# main("./day07/input")
+main("./day07/input")

4d4c812b093e5ccf33103175a8201c1ebcfee87f
Nicholas Amor <nick.amor@gmail.com>
2023-12-20 21:00:01 +1100

    day18 wip 2

--- day18/main.py
+++ day18/main.py
@@ -1,3 +1,5 @@
+import array
+from ctypes import Array
 import re
 from functools import reduce
 
@@ -11,31 +13,50 @@ def read_file(filename):
         return list(map(parse_plan_step, f.read().split("\n")[:-1]))
 
 
-def dig(coords=(0, 0), dig_map=[[""]]):
-    new_map = dig_map.copy()
+def dig_hole(coords, dig_map):
     (x, y) = coords
-    new_map[y][x] = "#"
-    return new_map
 
+    (rows, cols) = dig_map.size
+    if y + 1 > cols or x + 1 > rows:
+        dig_map = dig_map.resize((max(x + 1, rows), max(y + 1, cols)))
 
-def create_map(width, height):
-    dig_map = []
-    for h in range(height):
-        dig_map.append([])
-        for w in range(width):
-            dig_map[h].append(".")
-
+    dig_map.data[y][x] = "#"
     return dig_map
 
 
-dig_plan = read_file("./day18/test")
-print(dig_plan)
+def create_map():
+    class Grid:
+        def __init__(self, data=[], size=(0,0)) -> None:
+            self.data = data
+            self.size = size
 
-width = reduce(lambda acc, curr: acc + curr[1] if curr[0] == "R" else acc, dig_plan, 1)
-height = reduce(lambda acc, curr: acc + curr[1] if curr[0] == "D" else acc, dig_plan, 1)
-dig_map = create_map(width, height)
+        def resize(self, new_size):
+            (columns, rows) = self.size
+            (new_columns, new_rows)=new_size
+
+            new_data = [['.' for j in range(new_columns)] for i in range(new_rows)]
+
+            for i in range(new_rows):
+                for j in range(new_columns):
+                    new_data[i][j] = self.data[i][j] if i < rows and j < columns else '.'
+
+            # self.data=new_data
+            # self.size=new_size
+
+            return Grid(new_data, new_size)
+
+    return Grid()
+
+
+dig_plan = read_file("./day18/test")
+
+# width = reduce(lambda acc, curr: acc + curr[1] if curr[0] == "R" else acc, dig_plan, 1)
+# height = reduce(lambda acc, curr: acc + curr[1] if curr[0] == "D" else acc, dig_plan, 1)
+dig_map = create_map()
+
 current_coord = (0, 0)
-dig_map = dig(current_coord, dig_map)
+dig_map = dig_hole(current_coord, dig_map)
+
 for step in dig_plan:
     (direction, dist, _color) = step
     for i in range(dist):
@@ -49,6 +70,7 @@ for step in dig_plan:
         if direction == "R":
             x += 1
         current_coord = (x, y)
-        dig_map = dig(current_coord, dig_map)
-final_map = reduce(lambda acc, curr: , dig_map, "")
-print()
+        dig_map = dig_hole(current_coord, dig_map)
+
+final_map = reduce(lambda acc, curr: acc + str.join("", curr) + "\n", dig_map.data, "")
+print(final_map)
